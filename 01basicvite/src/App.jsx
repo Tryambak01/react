cd59acc921c48b8302e3015328f5d26ed54f5840
Tryambak01 <tryambakaadiga@gmail.com>
2023-12-22 14:53:53 +0530

    vite react

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,15 @@
+import Youtube from "./Youtube"
+
+//These functions are called components. Also , any file with .jsx returs html from react.
+
+function App() {
+  return (
+    <>
+        <h1>Classic Vite React App</h1>
+        <Youtube />
+    </>
+    
+  )
+}
+
+export default App
---
+++ 01basicvite/src/Youtube.jsx
@@ -0,0 +1,9 @@
+function Youtube() {
+    return (
+        <>
+            <h1>What was that madness before</h1>
+        </>
+    )
+}
+
+export default Youtube
